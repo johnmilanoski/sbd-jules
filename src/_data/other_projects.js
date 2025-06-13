f3c3dd49a089d856f9898cab7ac01099e64a2c68
johnmilanoski <30589725+johnmilanoski@users.noreply.github.com>
2025-06-13 00:25:28 -0700

    Add dynamic other projects pages

---
+++ src/_data/other_projects.js
@@ -0,0 +1,23 @@
+const loadGallery = require('./helpers/load-gallery');
+
+module.exports = async () => {
+  const folderToName = {
+    highland: 'Highland House',
+    magnolia: 'Magnolia House',
+    eastside_hotel: 'Eastside Hotel'
+  };
+
+  const folders = Object.keys(folderToName);
+
+  const gallery = await loadGallery('other', folders, {
+    thumbnailDir: 'other_index_pics'
+  });
+
+  return gallery.map(item => ({
+    name: folderToName[item.name] || item.name,
+    slug: item.name.replace(/_/g, '-'),
+    imageDir: item.imageDir,
+    thumbnail: item.thumbnail,
+    images: item.images
+  }));
+};
